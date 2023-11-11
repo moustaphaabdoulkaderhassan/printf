74358f6e09261b1e07991068da49798c3e289fae
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-11 04:17:19 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,60 +1,24 @@
-#include <unistd.h>
-#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 
-int print_char(char c) {
-    return write(1, &c, 1);
-}
+/**
+ * _printf - function my printf
+ * @format: string whit format to print
+ *
+ * Return: number of chars that print
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int length = 0;
 
-int print_str(const char *str) {
-    int n = 0;
-    while (*str) {
-        n += write(1, str++, 1);
-    }
-    return n;
-}
+	if (format == NULL)
+		return (-1);
 
-int print_binary(unsigned int num) {
-    char buffer[32];  // Assuming 32-bit integers
-    int bit, n = 0;
+	va_start(args, format);
 
-    for (bit = 31; bit >= 0; bit--) {
-        buffer[31 - bit] = ((num >> bit) & 1) + '0';
-    }
-
-    return write(1, buffer, 32);  // Adjust if needed
-}
-
-int _printf(const char *format, ...) {
-    if (format == NULL) {
-        return -1;
-    }
-
-    int n = 0;
-    va_list ap;
-    va_start(ap, format);
-
-    for (const char *p = format; *p; p++) {
-        if (*p != '%') {
-            n += print_char(*p);
-        } else {
-            p++;
-            if (*p == 's') {
-                n += print_str(va_arg(ap, const char *));
-            } else if (*p == 'c') {
-                n += print_char(va_arg(ap, int));
-            } else if (*p == 'b') {
-                n += print_binary(va_arg(ap, unsigned int));
-            }
-            // Add more cases for other format specifiers if needed
-        }
-    }
-
-    va_end(ap);
-
-    return n;
-}
-
-int main(void) {
-    _printf("Testing %s, %c, and %b\n", "Hello", 'A', 98);
-    return 0;
+	length = _print_format(format, args);
+	va_end(args);
+	return (length);
 }
