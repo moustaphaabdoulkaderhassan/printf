d9c678f3e16a33858a2db157044cba9d399744f5
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-11 04:20:42 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,24 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
-
 /**
- * _printf - function my printf
- * @format: string whit format to print
- *
- * Return: number of chars that print
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
-	va_list args;
-	int length = 0;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-	if (format == NULL)
-		return (-1);
+	va_list args;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	length = _print_format(format, args);
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
 	va_end(args);
-	return (length);
+	return (len);
 }
