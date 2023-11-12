f6e076a27a1a75d5208a0a10f5d2c5dd5d08bfdf
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-12 07:38:31 -0800

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,64 +1,20 @@
-#include <main.h>
+#include <limits.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    // Other declarations and test cases...
 
-    int count = 0; // to keep track of the number of characters printed
+    // Your specific statement
+    _printf("0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life\n");
 
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++; // move to the next character after '%'
+    // More test cases...
 
-            switch (*format) {
-                case 'c':
-                    // %c: Character
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                    
-                case 's':
-                    // %s: String
-                    {
-                        const char *str = va_arg(args, const char *);
-                        while (*str != '\0') {
-                            putchar(*str);
-                            str++;
-                            count++;
-                        }
-                    }
-                    break;
-
-                case '%':
-                    // %%: Percent sign
-                    putchar('%');
-                    count++;
-                    break;
-
-                default:
-                    // Unknown specifier, just print it as is
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-            }
-        } else {
-            // Regular character, just print it
-            putchar(*format);
-            count++;
-        }
-
-        format++; // move to the next character in the format string
-    }
-
-    va_end(args);
-    return count;
-}
-
-int main() {
-    // Example usage
-    _printf("Hello, %s! This is a %c example. The value of pi is %.2f%%.\n", "world", 'C', 3.14159);
-    
-    return 0;
+    return (0);
 }
