3be88773e3b319e1a7a6cccaf009a6eaf31f4422
Wandzfuture <michael.zabas@gmail.com>
2023-11-12 10:32:13 -0800

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -1,68 +1,52 @@
-
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
+ * _printf - is the Printf function
  * @format: format.
- * Return: Printed chars.
+ * Return: Printed characterss.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
 	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		return -1;
+	}
+
+	va_list ap;
+	va_start(ap, format);
+
+	int n = 0;
+	const char *p;
+
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p != '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
+			n += print_chr(*p);
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
+			p++;
+			if (*p == 's')
+			{
+				n += print_str(va_arg(ap, const char *));
+			}
+			else if (*p == 'c')
+			{
+				n += print_chr(va_arg(ap, int));
+			}
+			else if (*p == 'b')
+			{
+				n += print_binry(va_arg(ap, unsigned int));
+			}
+			else if (*p == 'i' || *p == 'd')
+			{
+				n += print_int(va_arg(ap, int));
+			}
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	va_end(ap);
 
-	va_end(list);
-
-	return (printed_chars);
+	return n;
 }
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
---
+++ functions.c
@@ -0,0 +1,83 @@
+#include "main.h"
+
+/**
+ * print_chr - prints a character
+ * Returns: The number of characters printed
+ */
+int print_chr(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_str - Prints a string.
+ * Return: Number of characters printed.
+ */
+int print_str(const char *str)
+{
+	int n = 0;
+
+	while (*str)
+	{
+		n += write(1, str++, 1);
+	}
+
+	return (n);
+}
+
+/**
+ * print_binry - is a function that Prints an unsigned number.
+ * Return: The numbers of characters printed.
+ */
+int print_binry(unsigned int num)
+{
+	char buffer[32];
+	int bit = 0;
+
+	for (bit = 31; bit >= 0; bit--)
+	{
+		buffer[31 - bit] = ((num >> bit) & 1) + '0';
+	}
+
+	return (write(1, buffer, 32));
+}
+
+/**
+ * print_int - prints an integer
+ * Return: Number of chars printed
+ */
+int print_int(int num)
+{
+	int n = 0;
+	int temp = num;
+	char buffer[12];
+	int chrs_printed = 0;
+
+	if (num < 0)
+	{
+		n += print_chr('-');
+		num = -num;
+		chrs_printed++;
+	}
+
+	while (temp > 0)
+	{
+		temp /= 10;
+		chrs_printed++;
+	}
+
+	if (chrs_printed == 0)
+	{
+		chrs_printed++;
+	}
+
+	for (int i = chrs_printed - 1; i >= 0; i--)
+	{
+		buffer[chrs_printed - 1 - i] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	n += write(1, buffer, chrs_printed);
+
+	return (n);
+}
