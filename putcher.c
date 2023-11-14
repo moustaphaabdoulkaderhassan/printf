4b7fad70a3acf8c39aa09573156b04fc2096d423
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-14 01:35:33 -0800

    _printf.c

--- putcher.c
+++ putcher.c
@@ -5,8 +5,10 @@
  * @c: The character to be written
  *
  * Return: On success, the number of characters written is returned.
- * On error, -1 is returned, and errno is set appropriately.
+ *         On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c) {
-    return write(1, &c, 1);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
+
