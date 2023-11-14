526528989914d3f411af7ea3c7a82c56eaf74b45
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-14 00:45:49 -0800

    _printf.c

--- main.h
+++ main.h
@@ -1,8 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Add libraries */
 #include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+/* Prototypes */
+int printChar(int ch);
+int printString(const char *string);
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
